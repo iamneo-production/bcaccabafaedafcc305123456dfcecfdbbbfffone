962704c9245da11016dd3c4b1a74fc851689366f
Iamneo <techadmin@iamneo.ai>
2023-09-14 10:04:17 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -2,6 +2,18 @@ function count()
 {
     let sentance = document.getElementById("text").value;
     let words = sentance.split(" ");  
-    if(length(words)
-    let special = []
+    let count = 0;
+    if(length(words) == 0)
+        window.alert("enter text properly");
+    let special = ['!','@','#','$','%','^','&','*','(',')','-','=','+']
+    for(let i of words)
+    {
+        if(i  in special)
+        {
+
+        }
+        else{
+                count+=1;
+        }
+    }
 }
